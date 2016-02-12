2ce00d0a506612ed633367cf3fde20bd68f30ef5
Hack Reactor Students <accounts@hackreactor.com>
2016-02-11 17:01:28 -0800

    Modify URL short code creation

--- app/config.js
+++ app/config.js
@@ -1,10 +1,13 @@
 var mongoose = require('mongoose');
 mongoose.connect('mongodb://localhost/shortly');
+var db = mongoose.connection;
 
 var Schema = mongoose.Schema;
 var ObjectId = Schema.ObjectId;
 
-var db = mongoose.connection;
+var crypto = require('crypto');
+var bcrypt = require('bcrypt-nodejs');
+
 
 db.on('error', console.error.bind(console, 'connection error:'));
 
@@ -17,14 +20,6 @@ var linkSchema = new Schema({
   visits: {type: Number, default: 0},
   timestamps: Date
 });
-
-linkSchema.methods.initialize = function() {
-  this.on('creating', function(model, attrs, options) {
-    var shasum = crypto.createHash('sha1');
-    shasum.update(model.get('url'));
-    model.set('code', shasum.digest('hex').slice(0, 5));
-  });
-};
 
 
 var userSchema = new Schema({
